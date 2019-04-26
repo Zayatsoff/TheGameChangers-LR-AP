ea1e80f3c6ddbba33c7c96b1e11a10f95001d88a
LIOR ROZIN <S347336653@OCDSB>
2019-04-26 15:23:47 -0400

    TextTilte

--- ObjectsJS/Zayats_Menus.js
+++ ObjectsJS/Zayats_Menus.js
@@ -1,19 +1,33 @@
-function splash() {
+class Splash {
+
+  constructor(){
+    this.x=width/2;
+    this.y=height/2;
+    this.size=40;
+    this.GO_UP = 1;
+  }
+
+  show(){
   background(220, 244, 240);
 
-  this.x = width /2;
-  this.y = height/2;
+ //this.x = width /2;
+  //this.y = height/2;
   /// Main_Text
-  textSize(70);
+  textSize(this.size);
   //textFont(edunline);
   fill(0);
   textAlign(CENTER);
   text('GameName!', this.x ,this.y);
-  ///
 
-   this.update = function() {
-    this.x++;
-    this.y++;
+}
+   update(){
+     this.size += this.GO_UP;
+     if (this.size < 40 || this.size > 70) {
+       this.GO_UP*=-1;
+
+     }
+
+
 
   }
 
--- main_sketch.js
+++ main_sketch.js
@@ -1,17 +1,19 @@
 
+let splash ;
+
 function setup() {
   createCanvas(550, 400);
-
+splash= new Splash();
 
 }
 
 /*function preload() {
 edunline = loadFont("Fonts\edunline.tff");
 }*/
-
 function draw() {
 
-splash();
-splash.update();
+  splash.update();
+  splash.show();
+
 
 }
