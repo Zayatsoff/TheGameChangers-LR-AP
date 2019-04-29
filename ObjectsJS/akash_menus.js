5d01ffeefdcaf0135c227c8fc30e8fa9eb4eadf3
akashprasad <47332388+AkashPrasad2@users.noreply.github.com>
2019-04-29 13:29:11 -0400

    aksMenu

---
+++ ObjectsJS/akash_menus.js
@@ -0,0 +1,36 @@
+function Menu() {
+background(53, 184, 198);
+  
+  //title
+  
+
+  fill(128, 0, 0);
+  //rect(20,5,350,175);
+  //fill(255,255,255)
+  //rect(23,8,5,5);
+  //ect(362,8,5,5);
+  //rect(362,172,5,5);
+  //rect(23,172,5,5);
+  
+  textSize(73);
+  fill(255,255,255);
+  text("Jump or ",22,69);
+  textSize(65);
+  text("Thump",30,140);
+  
+  
+  //Rules
+  textSize(30)
+  text("Instructions",200,245)
+  
+  //aboutUs
+  text("About us",200,280)
+  
+  //options
+  text("Settings",200,315)
+  
+  //Play button
+  text("Play!",200, 350);
+                                             
+ 
+}
