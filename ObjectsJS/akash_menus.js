e3de82bf1f7345d032e1f29825746c500b4d7638
akashprasad <47332388+AkashPrasad2@users.noreply.github.com>
2019-04-30 14:20:15 -0400

    added intructions

--- ObjectsJS/akash_menus.js
+++ ObjectsJS/akash_menus.js
@@ -34,3 +34,40 @@ background(53, 184, 198);
 
 
 }
+
+
+
+function aksInstructions() {
+  background(255)
+  
+  //How to play
+  textSize(40)
+  fill(255,0,0)
+  text("How to Play",150,50)
+  
+  //objective
+  textSize(25)
+  fill(0,0,255)
+  text("Objective:",30,85)
+  
+  textSize(20);
+  fill(0,0,0)
+  text("Controle your character throght the levels",15,108);
+  text("by avoiding obstacles and answering math ",15,128)
+  text("questions as you go.",16,148)
+  
+  
+  
+  //rules
+  fill(0,0,255)
+  textSize(25)
+  text("Rules:",30,200)
+  
+  fill(0,0,0)
+  textSize(20)
+  text("- Use arrow keys to move",15,225)
+  text("- Press 'space bar' to jump",15,245)
+  text("- Enter the correct value to aswer the",15,265)
+  text("question",15,285)
+  
+}
