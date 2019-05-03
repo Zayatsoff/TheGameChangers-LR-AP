a2eb8faa9ff9869333653e755be2e3fc4e2924aa
LIOR ROZIN <s347336653@ocdsb>
2019-05-03 13:14:34 -0400

    Update main_sketch.js

--- main_sketch.js
+++ main_sketch.js
@@ -1,6 +1,6 @@
 // keyCodes: http://keycode.info/
 let splash;
-var sceneNum = 0;
+
 function setup() {
   createCanvas(550, 400);
 splash = new Splash();
@@ -11,15 +11,15 @@ function draw() {
   if (SceneNum === 0) {
   aksMenu();
   }
-  
-push();
-splash.update();
-splash.show();
-pop();
-if(keyCode === 32 && sceneNum >= -1 && sceneNum <=1) {
-    aksMenu();
-  
-  
+
+// push();
+// splash.update();
+// splash.show();
+// pop();
+// if(keyCode === 32 && sceneNum >= -1 && sceneNum <=1) {
+//     aksMenu();
+
+
   function mouseClicked(){
   if (SceneNum === 0 && mouseX >= 180 && mouseX <= 335 && mouseY >= 255 && mouseY <= 295){
     aksInstructions();
