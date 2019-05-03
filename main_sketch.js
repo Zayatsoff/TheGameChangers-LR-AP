b790d830e33eec34ec96ef252df3f77d22001174
akashprasad <47332388+AkashPrasad2@users.noreply.github.com>
2019-05-03 13:34:22 -0400

    Update main_sketch.js

--- main_sketch.js
+++ main_sketch.js
@@ -8,9 +8,9 @@ splash = new Splash();
 }
 
 function draw() {
-  if (SceneNum === 0) {
+  if (SceneNum === 0){
   aksMenu();
-  }
+ }
 
 // push();
 // splash.update();
@@ -18,12 +18,14 @@ function draw() {
 // pop();
 // if(keyCode === 32 && SceneNum >= -1 && sceneNum <=1) {
 //     aksMenu();
-
-
-  function mouseClicked(){
-  if (SceneNum === 0 && mouseX >= 180 && mouseX <= 335 && mouseY >= 255 && mouseY <= 295){
-    aksInstructions();
-    SceneNum++;
-  }
 }
+
+function mouseClicked(){
+if (SceneNum === 0 && mouseX >= 180 && mouseX <= 335 && mouseY >= 255 && mouseY <= 295){
+  aksInstructions();
+  SceneNum = 1;
+}
+
+//if (SceneNum === 1 && mouse Y)
+
 }
