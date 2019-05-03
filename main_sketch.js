e45a3816d4f22d1bdb75fd1ac12894bac634b2bb
akashprasad <47332388+AkashPrasad2@users.noreply.github.com>
2019-05-03 13:11:54 -0400

    Update main_sketch.js

--- main_sketch.js
+++ main_sketch.js
@@ -18,5 +18,12 @@ splash.show();
 pop();
 if(keyCode === 32 && sceneNum >= -1 && sceneNum <=1) {
     aksMenu();
+  
+  
+  function mouseClicked(){
+  if (SceneNum === 0 && mouseX >= 180 && mouseX <= 335 && mouseY >= 255 && mouseY <= 295){
+    aksInstructions();
+    SceneNum++;
+  }
 }
 }
