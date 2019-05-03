3c298a0ab5a2bb0d25f9e1429384a02a3960295b
akashprasad <47332388+AkashPrasad2@users.noreply.github.com>
2019-05-03 13:03:36 -0400

    Update akash_menus.js

--- ObjectsJS/akash_menus.js
+++ ObjectsJS/akash_menus.js
@@ -1,7 +1,9 @@
-function aksMenu() {
-  currentScene= 1;
-background(53, 184, 198);
+fvar SceneNum = 0;
 
+function aksMenu() {
+  SceneNum= 1;
+background(53, 184, 198);
+  
   //title
   textSize(73);
   fill(255,255,255);
@@ -9,61 +11,47 @@ background(53, 184, 198);
   text("or",230,122)
   textSize(65);
   text("Thump!",155,180);
-
-
+  
+  
   //Rules
   textSize(30)
   rect(180,255,155,40)
   fill(0, 0, 0);
   text("Instructions",180,285)
-
+  
   //aboutUs
   text("About us",15,230)
-
+  
   //options
   text("Settings",400,245)
-
+  
   //Play button
   textSize(40)
   fill(255,0,0)
   text("Play!",215, 360);
-
-
+                                             
+ 
 }
+
 
 
 
 function aksInstructions() {
+  SceneNum = 2
   background(255)
-
+  
   //How to play
-  textSize(40);
-  fill(255, 0, 0);
-  text("How to Play", 150, 50);
-
+  textSize(50)
+  fill(255,0,0)
+  text("How to Play",140,50)
+  
   //objective
+  textSize(35)
+  fill(0,0,255)
+  text("Objective:",100,95)
+  
   textSize(25);
-  fill(0, 0, 255);
-  text("Objective:", 30, 85);
-
-  textSize(20);
-  fill(0, 0, 0);
-  text("Controle your character throght the levels", 15, 108);
-  text("by avoiding obstacles and answering math ", 15, 128);
-  text("questions as you go.", 16, 148);
-
-
-
-  //rules
-  fill(0, 0, 255);
-  textSize(25);
-  text("Rules:", 30, 200);
-
-  fill(0, 0, 0);
-  textSize(20);
-  text("- Use arrow keys to move", 15, 225);
-  text("- Press 'space bar' to jump", 15, 245);
-  text("- Enter the correct value to aswer the", 15, 265);
-  text("question", 15, 285);
-
-}
+  fill(0,0,0)
+  text("Controle your character throght the levels",15,118);
+  text("by avoiding obstacles and answering math ",15,138)
+  text("questions as you go.",16,158)
