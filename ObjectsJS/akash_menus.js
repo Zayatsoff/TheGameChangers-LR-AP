6fbfccbffcaf04d9b7338217c8bd534c6cf78621
LIOR ROZIN <S347336653@OCDSB>
2019-05-03 13:18:42 -0400

    app

--- ObjectsJS/akash_menus.js
+++ ObjectsJS/akash_menus.js
@@ -1,57 +1,54 @@
-var SceneNum = 0;
-
 function aksMenu() {
-  SceneNum= 1;
-background(53, 184, 198);
-  
+  SceneNum = 0;
+  background(53, 184, 198);
+
   //title
   textSize(73);
-  fill(255,255,255);
-  text("Jump",175,69);
-  text("or",230,122)
+  fill(255, 255, 255);
+  text("Jump", 175, 69);
+  text("or", 230, 122);
   textSize(65);
-  text("Thump!",155,180);
-  
-  
+  text("Thump!", 155, 180);
+
+
   //Rules
-  textSize(30)
-  rect(180,255,155,40)
+  textSize(30);
+  rect(180, 255, 155, 40);
   fill(0, 0, 0);
-  text("Instructions",180,285)
-  
+  text("Instructions", 180, 285);
+
   //aboutUs
-  text("About us",15,230)
-  
+  text("About us", 15, 230);
+
   //options
-  text("Settings",400,245)
-  
+  text("Settings", 400, 245);
+
   //Play button
-  textSize(40)
-  fill(255,0,0)
-  text("Play!",215, 360);
-                                             
- 
+  textSize(40);
+  fill(255, 0, 0);
+  text("Play!", 215, 360);
 }
 
 
 
 
 function aksInstructions() {
-  SceneNum = 2
+  SceneNum = 1
   background(255)
-  
+
   //How to play
   textSize(50)
-  fill(255,0,0)
-  text("How to Play",140,50)
-  
+  fill(255, 0, 0)
+  text("How to Play", 140, 50)
+
   //objective
   textSize(35)
-  fill(0,0,255)
-  text("Objective:",100,95)
-  
+  fill(0, 0, 255)
+  text("Objective:", 100, 95)
+
   textSize(25);
-  fill(0,0,0)
-  text("Controle your character throght the levels",15,118);
-  text("by avoiding obstacles and answering math ",15,138)
-  text("questions as you go.",16,158)
+  fill(0, 0, 0)
+  text("Controle your character throght the levels", 15, 118);
+  text("by avoiding obstacles and answering math ", 15, 138)
+  text("questions as you go.", 16, 158)
+}
--- main_sketch.js
+++ main_sketch.js
@@ -1,6 +1,6 @@
 // keyCodes: http://keycode.info/
 let splash;
-
+var SceneNum = 0;
 function setup() {
   createCanvas(550, 400);
 splash = new Splash();
@@ -16,7 +16,7 @@ function draw() {
 // splash.update();
 // splash.show();
 // pop();
-// if(keyCode === 32 && sceneNum >= -1 && sceneNum <=1) {
+// if(keyCode === 32 && SceneNum >= -1 && sceneNum <=1) {
 //     aksMenu();
 
 
