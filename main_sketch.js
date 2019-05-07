571a51cd5198b97534c1876f7fc0ea8b51a5368d
LIOR ROZIN <S347336653@OCDSB>
2019-05-07 14:01:37 -0400

    being a pirate

--- main_sketch.js
+++ main_sketch.js
@@ -12,20 +12,24 @@ function setup() {
 }
 
 function draw() {
+//Menu
   if (SceneNum === 1) {
     aksMenu();
   }
+  //Instructions
   else if (SceneNum === 2) {
     aksInstructions();
   }
+  //Play Screen1
   else if (SceneNum === 3) {
     aksPlayScreen();
   }
+  //About us
   else if (SceneNum === 6) {
     aksAboutUs();
   }
 
-
+//Splash
   else {
     push();
     splash.update();
@@ -36,7 +40,7 @@ function draw() {
     }
   }
 
-
+//Cofetti
   // aksCorrect();
   // for (var i = 0; i < 500; i++) {
   //   confet[i].display();
@@ -45,18 +49,22 @@ function draw() {
 }
 
 function mouseClicked() {
+  //Open Instructions from Menu
   if (SceneNum === 1 && mouseX >= 180 && mouseX <= 335 && mouseY >= 255 && mouseY <= 295) {
     aksInstructions();
     SceneNum = 2;
   }
+  //Back to Menu from Instructions
   else if (SceneNum === 2 && mouseX >= 30 && mouseX <= 90 && mouseY >= 325 && mouseY <= 375) {
     aksMenu();
       SceneNum = 1;
   }
+  //Open Play screen from Menu
   else if (SceneNum === 1 && mouseX >= 215 && mouseX <= 385 && mouseY >= 330 && mouseY <= 370) {
     aksPlayScreen();
     SceneNum = 3;
   }
+  //Open About us screen from Menu
   else if (SceneNum === 1 && mouseX >= 15 && mouseX <= 135 && mouseY >= 205-5 && mouseY <= 235) {
     aksAboutUs();
     SceneNum = 6;
