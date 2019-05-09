11339da0b826715be77623fb2977811e99ea6194
LIOR ROZIN <S347336653@OCDSB>
2019-05-09 13:59:37 -0400

    tags

--- main_sketch.js
+++ main_sketch.js
@@ -1,18 +1,22 @@
 // keyCodes: http://keycode.info/
+
+//Variables
 let splash;
 var confet = [];
 var SceneNum = 0;
 
 function setup() {
   createCanvas(550, 400);
+  //splash screen
   splash = new Splash();
+  //confetti
   for (var i = 0; i < 500; i++) {
     confet[i] = new Confetti();
   }
 }
 
 function draw() {
-//Menu
+  //Menu
   if (SceneNum === 1) {
     aksMenu();
   }
@@ -29,7 +33,7 @@ function draw() {
     aksAboutUs();
   }
 
-//Splash
+  //Splash
   else {
     push();
     splash.update();
@@ -40,7 +44,7 @@ function draw() {
     }
   }
 
-//Cofetti
+  //Cofetti
   // aksCorrect();
   // for (var i = 0; i < 500; i++) {
   //   confet[i].display();
@@ -57,7 +61,7 @@ function mouseClicked() {
   //Back to Menu from Instructions
   else if (SceneNum === 2 && mouseX >= 30 && mouseX <= 90 && mouseY >= 325 && mouseY <= 375) {
     aksMenu();
-      SceneNum = 1;
+    SceneNum = 1;
   }
   //Open Play screen from Menu
   else if (SceneNum === 1 && mouseX >= 215 && mouseX <= 385 && mouseY >= 330 && mouseY <= 370) {
@@ -65,12 +69,13 @@ function mouseClicked() {
     SceneNum = 3;
   }
   //Open About us screen from Menu
-  else if (SceneNum === 1 && mouseX >= 15 && mouseX <= 135 && mouseY >= 205-5 && mouseY <= 235) {
+  else if (SceneNum === 1 && mouseX >= 15 && mouseX <= 135 && mouseY >= 205 - 5 && mouseY <= 235) {
     aksAboutUs();
     SceneNum = 6;
   }
+  //Back to Menu from Instructions
   else if (SceneNum === 6 && mouseX >= 30 && mouseX <= 90 && mouseY >= 325 && mouseY <= 375) {
     aksMenu();
-      SceneNum = 1;
+    SceneNum = 1;
   }
 }
