eafd90f7f90688bd29d80b3da080fa8339d21703
Zayatsoff <47332155+Zayatsoff@users.noreply.github.com>
2019-06-20 14:43:08 -0400

    Revert "growth spurt"
    This reverts commit 796912b84b0fec69196bca4e8fc7e5e923fc1758.

--- ObjectsJS/Zayats_Menus.js
+++ ObjectsJS/Zayats_Menus.js
@@ -82,7 +82,6 @@ textSize(20)
 text(money + "$",30,height - 30)
 }
 /////////////////////////////////
-
 function playDev2() {
   SceneNum = 10;
   push();
@@ -107,22 +106,15 @@ function playDev2() {
 
   for (var i = 0; i < plat.length; i++) {
     plat[i].display();
-  player.CollidePlat(i);
+player.CollidePlat(i);
   }
   for (var i = 0; i < coin.length; i++) {
 
     player.CollideCoin(i);
   }
 
-  for (var i = 0; i < coin.length; i++) {
-    coin[i].display();
-  }
-
   player.display();
-  pop();
-  fill(255);
-  textSize(20)
-  text(money + "$",30,height - 30)
+
 }
 
 function keyPressed() {
