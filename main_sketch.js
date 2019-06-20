cc52c0b6c2187aa0fb75a382194176ce66b244d3
Zayatsoff <47332155+Zayatsoff@users.noreply.github.com>
2019-06-20 15:47:17 -0400

    Update main_sketch.js

--- main_sketch.js
+++ main_sketch.js
@@ -181,7 +181,41 @@ function mouseClicked() {
   } else if (SceneNum === 3 && mouseX >= 325 && mouseX <= 405 && mouseY >= 120 && mouseY <= 240) {
     OurChar = char2;
   }
+  /addition
+  if (sceneNum === 1 && mouseX <= 183 && mouseX >= 113 && mouseY >= 200 && mouseY <= 240) {
+    sceneNum += 3
+    Correct()
+  } else if (sceneNum === 1 && mouseX <= 325 && mouseX >= 256 && mouseY <= 240 && mouseY > 200) {
+    Wrong()
+  } else if (sceneNum === 1 && mouseY >= 200 && mouseY <= 240 && mouseX >= 399 && mouseX <= 469) {
+    Wrong()
+  }
 
+  // subtraction
+  else if (sceneNum === 2 && mouseX <= 183 && mouseX >= 113 && mouseY >= 200 && mouseY <= 240) {
+    sceneNum += 3
+    Wrong()
+  } else if (sceneNum === 2 && mouseX <= 325 && mouseX >= 256 && mouseY <= 240 && mouseY > 200) {
+    Correct()
+  } else if (sceneNum === 2 && mouseY >= 200 && mouseY <= 240 && mouseX >= 399 && mouseX <= 469) {
+    Wrong()
+  }
+
+  // multiplication
+  else if (sceneNum === 3 && mouseX <= 183 && mouseX >= 113 && mouseY >= 200 && mouseY <= 240) {
+    sceneNum += 3
+    Wrong()
+  } else if (sceneNum === 3 && mouseX <= 325 && mouseX >= 256 && mouseY <= 240 && mouseY > 200) {
+    Wrong()
+  } else if (sceneNum === 3 && mouseY >= 200 && mouseY <= 240 && mouseX >= 399 && mouseX <= 469) {
+    Correct()
+  }
+
+  //exit
+  else if (sceneNum === 20 || sceneNum === 21 || sceneNum === 22 && mouseX >= 40 && mouseX <= 120 && mouseY >= 300 && mouseY <= 340) {
+    sceneNum = 1;
+    aksMenu();
+  }
 
 
   // From win to next level
