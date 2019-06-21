4481fd95ed5ed1614940fa1f69949bdf65a926a7
Zayatsoff <47332155+Zayatsoff@users.noreply.github.com>
2019-06-20 21:51:37 -0400

    Update questions.js

--- ObjectsJS/questions.js
+++ ObjectsJS/questions.js
@@ -1,6 +1,6 @@
 class Questions {
   constructor() {
-    this.ASM = round(random(1, 3))
+    this.ASM = round(random(1, 3));
     //
     this.P1A = round(random(20, 50));
     this.P2A = round(random(20, 50));
@@ -14,20 +14,40 @@ class Questions {
     this.AA = this.P1A + this.P2A;
     this.SA = this.P1S - this.P2S;
     this.MA = this.P1M - this.P2M;
-
+    //
+    this.A = [this.AA, this.AA + 15, this.AA - 30];
+    this.S = [this.SA, this.SA + 15, this.SA - 30];
+    this.M = [this.MA, this.MA + 5, this.MA - 6];
+    //
+    this.shuffA = shuffle(this.A);
+    this.shuffS = shuffle(this.S);
+    this.shuffM = shuffle(this.M);
   }
+
+  //   once() {
+  //     buttons.shuffleADD(this.AA, this.AA + 15, this.AA - 30);
+  //   buttons.shuffleSUB(this.SA, this.SA + 15, this.SA - 30);
+  //         buttons.shuffleMULT(this.MA, this.MA + 5, this.MA - 6);
+
+  //   }
   display() {
     background(85, 105, 137);
     if (this.ASM === 1) {
-    this.add();
+      this.add();
+      this.butt();
+      this.text(this.shuffA)
+
     }
 
     if (this.ASM === 2) {
-    this.sub();
+      this.sub();
+      this.butt();
+      this.text(this.shuffS)
     }
-
     if (this.ASM === 3) {
-    this.mult();
+      this.mult();
+      this.butt();
+      this.text(this.shuffM)
     }
 
   }
@@ -47,7 +67,20 @@ class Questions {
     textSize(50)
     text(this.P1M + ' x ' + this.P2M + ' = ', 185, 100);
   }
+  butt() {
+    noStroke();
+    fill(133, 167, 221);
+    for (var i = 1; i <= 3; i++) {
+      rect(150 * i - 70, height / 2, 100, 50, 12)
+    }
+  }
 
+  text(array) {
+   fill(255);
 
+    text(array[0], 150  - 50, 245, 50);
+    text(array[1], 300 - 50, 245, 50);
+    text(array[2], 450 - 50,245, 50) ;
+  }
 
 }
